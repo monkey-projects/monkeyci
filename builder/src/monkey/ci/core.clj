07060bd9b4685a9eed296cdc39e64d197531d1e9
Wout Neirynck <wout@monkey-projects.be>
2023-07-12 11:40:25 +0200

    Check if script dir actually exists"

--- builder/src/monkey/ci/core.clj
+++ builder/src/monkey/ci/core.clj
@@ -1,17 +1,18 @@
 (ns monkey.ci.core
   "Core namespace for the Monkey CI app"
   (:gen-class)
-  (:require [clojure.tools.logging :as log]
+  (:require [clojure.java.io :as io]
+            [clojure.tools.logging :as log]
             [monkey.ci.process :as proc]))
 
 (defn -main
   "Main entry point for the application."
   [& [dir]]
-  (let [dir (or dir ".monkeyci")]
+  (let [dir (io/file (or dir ".monkeyci"))]
     (try
-      (when dir
+      (when (.exists dir)
         (log/info "Running build script at" dir)
-        (:exit (proc/execute! dir)))
+        (:exit (proc/execute! (.getAbsolutePath dir))))
       (finally
         ;; Shutdown the agents otherwise the app will block for a while here
         (shutdown-agents)))))
