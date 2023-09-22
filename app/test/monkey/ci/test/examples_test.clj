f869319e626b81d7a67601be5b6b7c100c800a19
Wout Neirynck <wout@monkey-projects.be>
2023-09-22 10:18:00 +0200

    Added example tests

---
+++ app/test/monkey/ci/test/examples_test.clj
@@ -0,0 +1,24 @@
+(ns monkey.ci.test.examples-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [clojure.tools.logging :as log]
+            [monkey.ci.core :as core]
+            [monkey.ci.test.helpers :as h]))
+
+(defn- run-example [path]
+  (log/info "Running example at" path)
+  (let [inv (-> core/build-cmd
+                :runs
+                (core/default-invoker {}))]
+    (inv {:workdir "examples"
+          :dir path
+          :dev-mode true})))
+
+(defn success? [r]
+  (= 0 (h/try-take r 20000 :timeout)))
+
+(deftest ^:integration examples
+  (testing "runs basic-clj example"
+    (is (success? (run-example "basic-clj"))))
+
+  (testing "runs basic-script example"
+    (is (success? (run-example "basic-script")))))
