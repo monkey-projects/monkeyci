d562c12859689f7a2a2ab6c7906a08d7f325c8f3
Wout Neirynck <wout@monkey-projects.be>
2023-10-18 17:03:36 +0200

    Cleaning up the build script a bit

--- .monkeyci/build.clj
+++ .monkeyci/build.clj
@@ -1,32 +1,19 @@
 ;; Build script for Monkey-ci itself
-(require '[monkey.ci.build.core :as core])
-(require '[monkey.ci.build.api :as api])
-(require '[monkey.ci.build.shell :as shell])
-(require '[clojure.java.io :as io])
-(require '[config.core :refer [env]])
-(require '[babashka.fs :as fs])
+(ns monkeyci.build.script
+  (:require [babashka.fs :as fs]
+            [monkey.ci.build
+             [api :as api]
+             [core :as core]
+             [shell :as shell]]))
 
-(defn set-name [s n]
-  (assoc s :name n))
-
-(defn clj [& args]
-  (apply str "clojure " args))
-
-(defn clj-container [dir script]
+(defn clj-container [name dir & args]
   "Executes script in clojure container"
-  {:container/image "docker.io/clojure:temurin-20-tools-deps-alpine"
-   :script (concat [(str "cd " dir)] script)})
+  {:name name
+   :container/image "docker.io/clojure:temurin-20-tools-deps-alpine"
+   :script [(str "cd " dir) (apply str "clojure " args)]})
 
-(defn clj-dir
-  "Runs `clojure` command in the given working dir"
-  [dir & args]
-  (clj-container dir [(apply clj args)]))
-
-(def clj-lib (partial clj-dir "lib"))
-(def clj-app (partial clj-dir "app"))
-
-(def test-lib (set-name (clj-lib "-X:test:junit") "test-lib"))
-(def test-app (set-name (clj-app "-M:test:junit") "test-app"))
+(def test-lib (clj-container "test-lib" "lib" "-X:test:junit"))
+(def test-app (clj-container "test-app" "app" "-M:test:junit"))
 
 (def app-uberjar (clj-app "-X:jar:uber"))
 
