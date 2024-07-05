42bfebc77f1cae7e03fef3c8eded79d563a69f01
Wout Neirynck <wout@monkey-projects.be>
2024-07-05 10:14:22 +0200

    Readers to loki

--- app/src/monkey/ci/logging/loki.clj
+++ app/src/monkey/ci/logging/loki.clj
@@ -3,11 +3,12 @@
    implementation, where promtail is problematic because it never shuts down."
   (:require [aleph.http :as http]
             [cheshire.core :as json]
+            [clojure.java.io :as io]
             [clojure.tools.logging :as log]
             [manifold.deferred :as md]
             [monkey.ci.logging :as l]))
 
-(defn post-logs
+(defn post-to-loki
   "Posts the given log streams to the configured endpoint"
   [{:keys [url tenant-id token]} streams]
   (letfn [(convert [s]
@@ -24,19 +25,64 @@
                            tenant-id (assoc "X-Scope-OrgID" tenant-id)
                            token (assoc "Authorization" (str "Bearer " token)))})))
 
+(defn post-lines
+  "Converts lines to a usable stream config for Loki and sends them out"
+  [lines {:keys [labels] :as conf}]
+  (when-not (empty? lines)
+    @(post-to-loki conf
+                   [(cond-> {:values lines}
+                      labels (assoc :stream labels))])))
+
+(defn post-or-acc
+  "Either posts all accumulated lines, or adds it to the list, depending
+   on configuration."
+  [l acc {:keys [threshold now] :as conf}]
+  (let [{:keys [lines timeout] :or {lines 100 timeout 1000}} threshold
+        total (conj acc l)
+        now (or now (System/currentTimeMillis))
+        threshold-reached? (or (>= (count total) lines)
+                               ;; TODO Keep track of last push time
+                               (>= now timeout))]
+    (if threshold-reached?
+      (do
+        (post-lines total conf)
+        [])
+      total)))
+
 (defn stream-to-loki
   "Pipes the given reader to Loki asynchronously.  Stops when the reader is
    closed.  Returns a deferred that resolves when the streaming stops."
   [reader conf]
   (let [r (java.io.LineNumberReader. reader)
-        res (md/deferred)]
-    (-> (Thread.
-         #(loop [l (.readLine r)]
-            (log/debug "Read line:" l)
-            (if l
-              (recur (.readline r))
-              (md/success! res {:line-count (.getLineNumber r)}))))
-        (.start))
+        read-line (fn []
+                    (try
+                      (some->> (.readLine r)
+                               (vector (System/currentTimeMillis)))
+                      (catch Exception ex
+                        (log/debug "Cause:" (ex-cause ex))
+                        (when (not= "Pipe closed" (ex-message ex))
+                          (log/warn "Failed to read next line" ex))
+                        nil)))
+        res (md/deferred)
+        thread (Thread.
+                #(loop [l (read-line)
+                        acc []]
+                   ;; TODO We should also post if no new line has been received for a while
+                   (log/debug "Read line:" l)
+                   (if l
+                     (recur (read-line)
+                            (post-or-acc l acc conf))
+                     (do
+                       (post-lines acc conf)
+                       (log/debug "Terminating stream loop")
+                       (md/success! res {:line-count (.getLineNumber r)})))))
+        interrupt (fn [_]
+                    (when (.isAlive thread)
+                      (log/debug "Interrupting streaming thread")
+                      (.interrupt thread)))]
+    (.start thread)
+    ;; Ensure thread terminates
+    (md/on-realized res (constantly nil) interrupt)
     res))
 
 (defrecord LokiLogger [conf rt path]
@@ -45,5 +91,4 @@
     :stream)
   
   (handle-stream [this in]
-    ;; TODO Upload to loki
-    ))
+    (stream-to-loki (io/reader in) conf)))
--- app/test/unit/monkey/ci/logging/loki_test.clj
+++ app/test/unit/monkey/ci/logging/loki_test.clj
@@ -7,19 +7,19 @@
             [monkey.ci.logging :as l])
   (:import [java.io PipedReader PipedWriter PrintWriter]))
 
-(deftest post-logs
+(deftest post-to-loki
   (testing "sends request"
     (with-redefs [http/request (fn [req]
                                  (md/success-deferred {:status 200
                                                        :body {:request req}}))]
-      (let [r (sut/post-logs {:url "http://loki"
-                              :tenant-id "test-tenant"
-                              :token "test-token"}
-                             [{:stream
-                               {:label "value"}
-                               :values
-                               [[100 "Line 1"]
-                                [200 "Line 2"]]}])]
+      (let [r (sut/post-to-loki {:url "http://loki"
+                                 :tenant-id "test-tenant"
+                                 :token "test-token"}
+                                [{:stream
+                                  {:label "value"}
+                                  :values
+                                  [[100 "Line 1"]
+                                   [200 "Line 2"]]}])]
         (is (some? r))
         (is (= 200 (:status @r)))
 
@@ -43,15 +43,74 @@
           (is (= "Bearer test-token" (get-in @r [:body :request :headers "Authorization"]))))))))
 
 (deftest stream-to-loki
-  (testing "posts all entries to loki asynchronously"
-    (let [inv (atom [])
-          reader (PipedReader.)
-          writer (PipedWriter. reader)
-          pw (PrintWriter. writer)]
-      (with-redefs [sut/post-logs (fn [_ streams]
-                                    (swap! inv conj streams))]
-        (let [r (sut/stream-to-loki reader {})]
-          (is (md/deferred? r))
-          (is (nil? (.close reader)))
-          (is (nil? (.close writer)))
+  (let [inv (atom [])
+        reader (PipedReader.)
+        writer (PipedWriter. reader)
+        pw (PrintWriter. writer)]
+    (with-redefs [sut/post-to-loki (fn [_ streams]
+                                     (swap! inv conj streams))]
+      (let [r (sut/stream-to-loki reader {})]
+        (is (md/deferred? r))
+        (is (some? (md/timeout! r 1000))) ; Safety
+        (is (nil? (.close reader)))
+        (is (nil? (.close writer)))
+        
+        (testing "resolves on reader close"
+          (is (map? @r)))
+
+        (testing "returns number of posted lines"
           (is (= 0 (:line-count @r))))))))
+
+(deftest post-lines
+  (with-redefs [sut/post-to-loki (fn [conf streams]
+                                (md/success-deferred
+                                 {:conf conf
+                                  :streams streams}))]
+    (testing "posts lines to loki"
+      (let [conf {:url "http://loki"}
+            lines [[100 "line 1"]
+                   [200 "line 2"]]
+            r (sut/post-lines lines conf)]
+        (is (= conf (:conf r)))
+        (is (= [{:values lines}]
+               (:streams r)))))
+
+    (testing "passes labels from config"
+      (let [labels {:key "value"}]
+        (is (= labels (-> (sut/post-lines [[100 "test line"]] {:labels labels})
+                          :streams
+                          first
+                          :stream)))))
+
+    (testing "does nothing if no data"
+      (is (nil? (-> (sut/post-lines [] {})))))))
+
+(defn with-fake-loki* [posts f]
+  (with-redefs [sut/post-to-loki (fn [_ streams]
+                                   (swap! posts conj streams)
+                                   (md/success-deferred {:status 200}))]
+    (f)))
+
+(defmacro with-fake-loki [posts & body]
+  `(let [~posts (atom [])]
+     (with-fake-loki* ~posts (fn [] ~@body))))
+
+(deftest post-or-acc
+  (testing "posts if max lines reached"
+    (with-fake-loki posts
+      (is (empty? (sut/post-or-acc [200 "New line"]
+                                   [[100 "Previous line"]]
+                                   {:threshold {:lines 2}})))
+      (is (= 1 (count @posts)))
+      (is (= ["Previous line" "New line"]
+             (map second (:values (ffirst @posts)))))))
+
+  (testing "posts if timeout reached"
+    (with-fake-loki posts
+      (is (empty? (sut/post-or-acc [200 "New line"]
+                                   [[100 "Previous line"]]
+                                   {:threshold {:timeout 1000}
+                                    :now 2000})))
+      (is (= 1 (count @posts)))
+      (is (= ["Previous line" "New line"]
+             (map second (:values (ffirst @posts))))))))
