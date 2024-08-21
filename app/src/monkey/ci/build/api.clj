2dd092e0e5a3ff206dea2e3f027a3e71bb1dfc40
Wout Neirynck <wout@monkey-projects.be>
2024-08-21 11:39:34 +0200

    More refactoring

--- app/src/monkey/ci/build/api.clj
+++ app/src/monkey/ci/build/api.clj
@@ -1,16 +1,18 @@
 (ns monkey.ci.build.api
-  "Functions for invoking the script API."
+  "Functions for invoking the build script API."
   (:require [clojure.tools.logging :as log]
-            [martian.core :as martian]
             [monkey.ci.build :as b]))
 
 (def rt->api-client (comp :client :api))
+
+(defn- repo-path [rt]
+  (apply format "/customer/%s/repo/%s" (b/get-sid rt)))
 
 (defn- fetch-params* [rt]
   (let [client (rt->api-client rt)
         [cust-id repo-id] (b/get-sid rt)]
     (log/debug "Fetching repo params for" cust-id repo-id)
-    (->> @(client {:url (format "/customer/%s/repo/%s/param" cust-id repo-id)
+    (->> @(client {:url (str (repo-path rt) "/param")
                    :method :get})
          (map (juxt :name :value))
          (into {}))))
@@ -25,5 +27,5 @@
   (let [client (rt->api-client rt)
         sid (b/get-sid rt)]
     (log/debug "Downloading artifact for build" sid ":" id)
-    @(client {:url (apply format "/customer/%s/repo/%s/builds/%s/artifact/%s/download" (conj sid id))
+    @(client {:url (format (str (repo-path rt) "/builds/%s/artifact/%s/download") (last sid) id)
               :method :get})))
