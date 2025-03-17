6ca775369514292497115de8e71c382d28cddb1a
Wout Neirynck <wout@monkey-projects.be>
2025-03-17 10:22:54 +0100

    Parse edn, json and yaml files as well

--- app/src/monkey/ci/script/core.clj
+++ app/src/monkey/ci/script/core.clj
@@ -1,38 +1,70 @@
 (ns monkey.ci.script.core
-  (:require [clojure.java.io :as io]
+  (:require [babashka.fs :as fs]
+            [camel-snake-kebab.core :as csk]
+            [cheshire.core :as json]
+            [clj-yaml.core :as yaml]
+            [clojure.java.io :as io]
             [clojure.tools.logging :as log]
             [monkey.ci
              [build :as build]
+             [edn :as edn]
              [jobs :as j]]
             [monkey.ci.build.core :as bc]))
 
 ;;; Script loading
 
-(defn- load-script
-  "Loads the jobs from the build script, by reading the script files 
-   dynamically.  If the build script does not define its own namespace,
-   one will be randomly generated to avoid collisions."
-  [dir build-id]
+(def extensions
+  "Accepted script extensions, in order of processing"
+  ["yaml" "json" "edn" "clj"])
+
+(defn- infer-type [job]
+  ;; Currently, only container jobs are supported in non-clj files
+  (assoc job :type :container))
+
+(defn- load-edn [path]
+  (->> (edn/edn-> path)
+       (map infer-type)))
+
+(defn- load-json [path]
+  (with-open [r (io/reader path)]
+    (->> (json/parse-stream r csk/->kebab-case-keyword)
+         (map infer-type))))
+
+(defn- load-yaml [path]
+  (-> (slurp path)
+      (yaml/parse-string :key-fn (comp csk/->kebab-case-keyword :key))
+      (as-> x (map infer-type x))))
+
+(defn- load-clj
+  "Loads the jobs from the build script, by reading the script files dynamically."
+  [path]
   ;; Don't wrap in ns, since `in-ns` may throw an exception at runtime if it
   ;; can't rebind `*ns*`
-  (let [path (io/file dir "build.clj")]
-    (log/debug "Loading script:" path)
-    ;; This should return jobs to run
-    (load-file (str path)))
-  #_(let [tmp-ns (symbol (or build-id (str "build-" (random-uuid))))]
-      ;; Declare a temporary namespace to load the file in, in case
-      ;; it does not declare an ns of it's own.
-      (in-ns tmp-ns)
-      #_(clojure.core/use 'clojure.core)
-      (try
-        (let [path (io/file dir "build.clj")]
-          (log/debug "Loading script:" path)
-          ;; This should return jobs to run
-          (load-file (str path)))
-        (finally
-          ;; Return
-          (in-ns 'monkey.ci.script)
-          (remove-ns tmp-ns)))))
+  (log/debug "Loading script:" path)
+  ;; This should return jobs to run
+  (load-file (str path)))
+
+(def loaders
+  "Script loader per extension"
+  {"edn"  load-edn
+   "json" load-json
+   "yaml" load-yaml
+   "yml"  load-yaml
+   "clj"  load-clj})
+
+(defn- load-script
+  "Loads the jobs defined in the script file, which could be declarative (yaml, 
+   json or edn) or a clj source file."
+  [dir ext]
+  (let [f (fs/path dir (str "build." ext))
+        l (get loaders ext)]
+    (when (fs/exists? f)
+      (log/debug "Loading jobs from" f)
+      (l (fs/file f)))))
+
+(defn- load-scripts [dir]
+  (->> extensions
+       (map (partial load-script dir))))
 
 (defn- assign-ids
   "Assigns an id to each job that does not have one already."
@@ -58,6 +90,6 @@
 (defn load-jobs
   "Loads the script and resolves the jobs"
   [build rt]
-  (-> (load-script (build/script-dir build) (build/build-id build))
+  (-> (load-scripts (build/script-dir build))
       (resolve-jobs rt)))
 
