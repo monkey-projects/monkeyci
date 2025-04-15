c5faf300634b6bfd612783af462a0fecff884503
Wout Neirynck <wout@monkey-projects.be>
2025-04-15 10:08:20 +0200

    Run build agent from main

--- app/src/monkey/ci/agent/main.clj
+++ app/src/monkey/ci/agent/main.clj
@@ -10,11 +10,18 @@
    It may be possible to suspend the build VMs after a certain inactivity timeout, to
    conserve computing capacity."
   (:gen-class)
-  (:require [clojure.tools.logging :as log]))
+  (:require [clojure.tools.logging :as log]
+            [monkey.ci.agent.runtime :as ar]
+            [monkey.ci.config :as c]
+            [monkey.ci.runtime.common :as rc]
+            [monkey.ci.web.http :as wh]))
 
 (defn -main [& args]
   (try
     (log/info "Starting build agent")
-    ;; TODO
+    (rc/with-system
+      (ar/make-system (c/load-config-file (first args)))
+      (fn [sys]
+        (wh/on-server-close (:api-server sys))))
     (finally
       (log/info "Build agent terminated."))))
--- app/test/unit/monkey/ci/agent/main_test.clj
+++ app/test/unit/monkey/ci/agent/main_test.clj
@@ -1,7 +1,17 @@
 (ns monkey.ci.agent.main-test
   (:require [clojure.test :refer [deftest testing is]]
-            [monkey.ci.agent.main :as sut]))
+            [babashka.fs :as fs]
+            [monkey.ci.agent.main :as sut]
+            [monkey.ci.web.http :as wh]
+            [monkey.ci.test
+             [config :as tc]
+             [helpers :as h]]))
 
 (deftest main
-  (testing "starts agent runtime"
-    (is (nil? (sut/-main {})))))
+  (with-redefs [wh/on-server-close (fn [s]
+                                     (.close (:server s)))]
+    (h/with-tmp-dir dir
+      (let [config-file (fs/path dir "config.edn")]
+        (is (nil? (spit (fs/file config-file) (pr-str tc/base-config))))
+        (testing "starts agent runtime"
+          (is (nil? (sut/-main (str config-file)))))))))
