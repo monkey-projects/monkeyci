87b134458bbb952c87e0d281e433d9d47c12b128
Wout Neirynck <wout@monkey-projects.be>
2025-06-19 09:06:41 +0200

    Stream and info implementations for s3 blobs

--- app/src/monkey/ci/blob/s3.clj
+++ app/src/monkey/ci/blob/s3.clj
@@ -3,16 +3,23 @@
   (:require [amazonica.aws.s3 :as s3]
             [babashka.fs :as fs]
             [clojure.tools.logging :as log]
+            [java-time.api :as jt]
             [manifold.deferred :as md]
+            [medley.core :as mc]
             [monkey.ci.blob.common :as c]
             [monkey.ci.build.archive :as a]
             [monkey.ci
              [protocols :as p]
              [utils :as u]]))
 
+;; Configuration contains all settings needed to connect to the bucket endpoint,
+;; including bucket-name.
 (defrecord S3BlobStore [conf]
   p/BlobStore
   (save-blob [this src dest md]
+    ;; We could also drop the intermediate file and use piped streams instead, which
+    ;; would be a bit faster and would not require disk space.  But then we'd need
+    ;; an additional thread.
     (let [tmp (c/tmp-archive conf)
           r (c/make-archive src tmp)
           details {:bucket-name (:bucket-name conf)
@@ -20,19 +27,41 @@
                    :file (u/abs-path tmp)
                    :metadata md}]
       (log/debug "Uploading to bucket:" details)
-      (-> (s3/put-object conf details)
+      (-> (md/future (s3/put-object conf details))
           (md/chain (constantly r))
           (md/finally #(fs/delete tmp)))))
   
   (restore-blob [this src dest]
     (log/debug "Restoring blob from bucket" (:bucket-name conf) ", path" src "to" dest)
-    (let [res (s3/get-object conf (:bucket-name conf) src)]
+    (let [res (md/future (s3/get-object conf (:bucket-name conf) src))]
       (-> res
           (md/chain #(a/extract (:input-stream %) dest))
-          (md/finally #(.close (:input-stream res))))))
+          (md/finally #(.close (:input-stream @res))))))
   
-  (get-blob-stream [this src])
+  (get-blob-stream [this src]
+    (log/debug "Downloading stream from bucket" (:bucket-name conf) "at" src)
+    (-> (s3/get-object conf (:bucket-name conf) src)
+        (md/future)
+        (md/chain :input-stream)))
   
-  (put-blob-stream [this src dest])
+  (put-blob-stream [this src dest]
+    (let [details {:bucket-name (:bucket-name conf)
+                   :key dest
+                   :input-stream src}]
+      (log/debug "Uploading stream to bucket:" details)
+      (-> (s3/put-object conf details)
+          (md/future)
+          (md/chain (fn [_]
+                      {:src src
+                       :dest dest})))))
   
-  (get-blob-info [this src]))
+  (get-blob-info [this src]
+    (-> (s3/get-object-metadata conf {:bucket-name (:bucket-name conf) :key src})
+        (md/future)
+        (md/chain (fn [res]
+                    (-> {:src src
+                         :size (:content-length res)
+                         :metadata (:user-metadata res)
+                         :result res}
+                        (merge (select-keys res [:last-modified :content-type]))
+                        (mc/update-existing :last-modified jt/instant)))))))
--- app/test/unit/monkey/ci/blob/s3_test.clj
+++ app/test/unit/monkey/ci/blob/s3_test.clj
@@ -2,10 +2,16 @@
   (:require [clojure.test :refer [deftest testing is]]
             [amazonica.aws.s3 :as s3]
             [babashka.fs :as fs]
+            [clj-commons.byte-streams :as bs]
             [clojure.java.io :as io]
-            [monkey.ci.blob :as blob]
+            [monkey.ci
+             [blob :as blob]
+             [protocols :as p]]
             [monkey.ci.blob.s3 :as sut]
+            [monkey.ci.build.archive :as a]
             [monkey.ci.test.helpers :as h]))
+
+(def input-stream? (partial instance? java.io.InputStream))
 
 (deftest s3-blob-store
   (h/with-tmp-dir dir
@@ -14,8 +20,10 @@
                                         :bucket-name "test-bucket"}}
                                       :store)
           src (str (fs/create-dirs (fs/path dir "src")))
-          archive-dir (str (fs/create-dirs (fs/path dir "archives")))]
+          archive-dir (str (fs/create-dirs (fs/path dir "archives")))
+          arch (fs/file (fs/path archive-dir "to-restore.tgz"))]
       (is (nil? (spit (fs/file (fs/path src "test.txt")) "test file")))
+      (is (some? (blob/make-archive src arch)))
       
       (testing "can create store"
         (is (blob/blob-store? store)))
@@ -36,9 +44,7 @@
 
       (testing "`restore-blob` downloads and extracts"
         (let [inv (atom nil)
-              arch (fs/file (fs/path archive-dir "to-restore.tgz"))
               dest (fs/path dir "extracted")]
-          (is (some? (blob/make-archive src arch)))
           (with-redefs [s3/get-object (fn [opts bucket path]
                                         (reset! inv {:metadata
                                                      {:opts opts
@@ -48,4 +54,50 @@
             (is (some? @(blob/restore store "test/src" (str dest))))
             (is (fs/exists? dest))
             (is (= "test file" (slurp (str (fs/path dest "test.txt")))))
-            (is (some? @inv))))))))
+            (is (some? @inv)))))
+
+      (testing "`get-blob-stream` returns blob as input stream"
+        (let [dest (fs/path dir "extracted")]
+          (with-redefs [s3/get-object (fn [opts bucket path]
+                                        {:input-stream (io/input-stream arch)})]
+            (with-open [res @(p/get-blob-stream store "test/src")]
+              (is (input-stream? res))
+              (is (some? (a/extract res (str dest))))
+              (is (= "test file" (slurp (str (fs/path dest "test.txt")))))))))
+
+      (testing "`put-blob-stream` directly uploads the stream to bucket"
+        (let [inv (atom nil)]
+          (with-redefs [s3/put-object (fn [opts details]
+                                        (reset! inv {:metadata
+                                                     {:opts opts
+                                                      :details details}}))]
+            (testing "puts archive in bucket"
+              (let [stream (java.io.ByteArrayInputStream. (.getBytes "test stream"))]
+                (is (some? @(p/put-blob-stream store stream "test/dest")))
+                (is (some? @inv))
+                (let [details (-> @inv :metadata :details)]
+                  (is (= "test-bucket" (:bucket-name details)))
+                  (is (= "test/dest" (:key details)))
+                  (is (= stream (:input-stream details)))))))))
+
+      (testing "`get-blob-info`"
+        (let [inv (atom nil)]
+          (with-redefs [s3/get-object-metadata
+                        (fn [opts details]
+                          (reset! inv {:opts opts
+                                       :details details
+                                       ;; s3 lib uses joda time
+                                       :last-modified (org.joda.time.DateTime.)}))]
+            (let [res @(p/get-blob-info store "test/src")]
+              (testing "fetches object metadata"
+                (is (some? @inv)))
+
+              (testing "returns original result in response"
+                (is (= {:bucket-name "test-bucket"
+                        :key "test/src"}
+                       (-> res
+                           :result
+                           :details))))
+
+              (testing "converts last-modified to java instant"
+                (is (instance? java.time.Instant (:last-modified res)))))))))))
