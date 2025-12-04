d74115e163be5b382a167a043b1df8dbeec6bc61
Wout Neirynck <wout@monkey-projects.be>
2025-12-04 16:01:10 +0100

    Mailings checkbox

--- gui/src/monkey/ci/gui/user/views.cljs
+++ gui/src/monkey/ci/gui/user/views.cljs
@@ -5,16 +5,19 @@
             [monkey.ci.gui.tabs :as tabs]
             [re-frame.core :as rf]))
 
-(defn- form-control [id lbl val & [input-opts]]
+(defn- form-row [lbl input]
   [:div.row
-   [:div.col-2
-    [:label.form-label {:for id} lbl]]
-   [:div.col-10
-    [:input.form-control
-     (merge {:name id
-             :id id
-             :value val}
-            input-opts)]]])
+   [:div.col-2 lbl]
+   [:div.col-10 input]])
+
+(defn- form-control [id lbl val & [input-opts]]
+  (form-row
+   [:label.form-label {:for id} lbl]
+   [:input.form-control
+    (merge {:name id
+            :id id
+            :value val}
+           input-opts)]))
 
 (defn general-page []
   (let [u (rf/subscribe [:login/user])]
@@ -22,7 +25,13 @@
      [co/page-title [:span.me-2 [co/icon :person-fill-gear]] "General Settings"]
      [:form.d-flex.flex-column.gap-2
       [form-control :name "Name" (:name @u) {:readonly true}]
-      [form-control :name "E-mail" (:email @u)]]]))
+      [form-control :name "E-mail" (:email @u)]
+      [:div.row
+       [:div.offset-2.col-10
+        [:div.form-check
+         [:input.form-check-input {:type :checkbox
+                                   :id :receive-mailing}]
+         [:label.form-check-label {:for :receive-mailing} "Receive mailings"]]]]]]))
 
 (defn tabs [id]
   [tabs/settings-tabs
