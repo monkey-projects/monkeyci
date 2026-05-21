6340234f5795e6c8a083328ec055f4373f447717
Wout Neirynck <wout@monkey-projects.be>
2026-05-21 14:57:32 +0200

    Basic script api client

--- script/bb/test_runner.clj
+++ script/bb/test_runner.clj
@@ -4,7 +4,8 @@
 
 (defn run-tests []
   (cp/add-classpath "src:test")
-  (require 'monkey.ci.script.core-test
+  (require 'monkey.ci.script.api-client-test
+           'monkey.ci.script.core-test
            'monkey.ci.script.config-test)
   (let [{:keys [fail error]} (t/run-all-tests (re-pattern "^monkey.ci.script.*-test$"))]
     (when (pos? (+ fail error))
