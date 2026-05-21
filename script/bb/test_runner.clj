236f3d07e152d3063749185db493d8e47b73f989
Wout Neirynck <wout@monkey-projects.be>
2026-05-21 13:23:34 +0200

    Enabled bb tests for script

---
+++ script/bb/test_runner.clj
@@ -0,0 +1,11 @@
+(ns test-runner
+  (:require [clojure.test :as t]
+            [babashka.classpath :as cp]))
+
+(defn run-tests []
+  (cp/add-classpath "src:test")
+  (require 'monkey.ci.script.core-test
+           'monkey.ci.script.config-test)
+  (let [{:keys [fail error]} (t/run-all-tests (re-pattern "^monkey.ci.script.*-test$"))]
+    (when (pos? (+ fail error))
+      (System/exit 1))))
--- script/src/monkey/ci/script/yaml.clj
+++ script/src/monkey/ci/script/yaml.clj
@@ -2,12 +2,14 @@
   "Functions for loading yaml script files"
   (:require [camel-snake-kebab.core :as csk]
             [clj-yaml.core :as yaml]
+            [flatland.ordered.map :as om]
             [monkey.ci.script
              [jobs :as j]
              [utils :as u]]))
 
 ;; Required for yaml
-(extend-type flatland.ordered.map.OrderedMap
+;; Retrieve the ordered map class using this workaround for Babashka
+(extend-type (class (om/ordered-map))
   j/JobResolvable
   (resolve-jobs [m rt]
     (when (j/job? m) [m])))
