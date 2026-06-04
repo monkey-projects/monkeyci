b05e776e84796653719292baad4d5b244d7a8960
Wout Neirynck <wout@monkey-projects.be>
2026-06-04 08:46:07 +0200

    Moved extensions to core + added core bb tests

---
+++ core/src/monkey/ci/extensions.clj
@@ -0,0 +1,108 @@
+(ns monkey.ci.extensions
+  "Functionality for working with script extensions.  Extensions are a way
+   for third party libraries to add functionality to scripts, that is easy
+   to activate and can also be used in yaml-type scripts.  You could of 
+   course also add regular functions to invoke, but this is not easy to
+   use, especially when using container jobs.  Extensions do this by registering
+   themselves under a specific namespaced keyword.  If this key is found in
+   job properties, the associated extension code is executed.  Extensions can
+   be executed before or after a job (or both)."
+  (:require [clojure.tools.logging :as log]
+            [monkey.ci.events.mailman.interceptors :as emi]))
+
+(def new-register {})
+
+(defonce registered-extensions (atom new-register))
+
+(defn register [l ext]
+  (assoc l (:key ext) ext))
+
+(defn register! [ext]
+  (swap! registered-extensions register ext))
+
+(defmacro with-extensions
+  "Convenience macro that executes the body with its own extensions (which
+   are initially empty).  Useful for testing."
+  [& body]
+  `(let [ext# @sut/registered-extensions]
+     (try
+       (reset! sut/registered-extensions sut/new-register)
+       ~@body
+       (finally
+         (reset! sut/registered-extensions ext#)))))
+
+(defmulti before-job (fn [k _] k))
+
+(defmulti after-job (fn [k _] k))
+
+(defn- find-mm
+  "Finds multimethod for `mm` and given key, returns a fn that invokes it with runtime arg."
+  [k mm]
+  (when-let [m (get-method mm k)]
+    #(m k %)))
+
+(defn- run-safe [f ctx]
+  (try
+    (f ctx)
+    (catch Exception ex
+      (log/error "Failed to apply extension" ex)
+      ;; Return context unchanged
+      ctx)))
+
+(defn- apply-extensions [{:keys [job] :as ctx} registered rk mm]
+  (->> (keys job)
+       (reduce (fn [r k]
+                 (let [b (or (get-in registered [k rk])
+                             (find-mm k mm))]
+                   (cond->> r
+                     b (run-safe b))))
+               ctx)))
+
+(defn apply-extensions-before
+  ([ctx registered]
+   (apply-extensions ctx registered :before before-job))
+  ([ctx]
+   (apply-extensions-before ctx @registered-extensions)))
+
+(defn apply-extensions-after
+  ([ctx registered]
+   (apply-extensions ctx registered :after after-job))
+  ([ctx]
+   (apply-extensions-after ctx @registered-extensions)))
+
+;;; Utility functions
+
+(defn get-config
+  "Retrieves configuration for the extension from the job"
+  [ctx k]
+  (get-in ctx [:job k]))
+
+(defn update-job
+  "Applies `f` with args to the job in the given job context.  This is useful for before
+   interceptors to modify the job that will be executed."
+  [ctx f & args]
+  (apply update ctx :job f args))
+
+(defn set-value
+  "Sets the extension value in the job.  This value will be stored with the job, so it
+   must be serializable to `edn`."
+  [ctx k v]
+  (update-job ctx assoc-in [:result k] v))
+
+;;; Interceptors
+
+(def before-interceptor
+  "Interceptor that applies the `before` extensions to the job in the job context.
+   This expects the job context to be present in the event context, with the job
+   added to that context."
+  {:name ::before
+   :enter (fn [ctx]
+            (emi/update-job-ctx ctx apply-extensions-before))})
+
+(def after-interceptor
+  "Interceptor that applies the `after` extensions to the job in the job context.
+   This expects the job context to be present in the event context, with the job
+   and it's execution result added to that context."
+  {:name ::after
+   :enter (fn [ctx]
+            (emi/update-job-ctx ctx apply-extensions-after))})
---
+++ core/test/monkey/ci/extensions_test.clj
@@ -0,0 +1,132 @@
+(ns monkey.ci.extensions-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [monkey.ci.build.core :as bc]
+            [monkey.ci.events.mailman.interceptors :as emi]
+            [monkey.ci.extensions :as sut :refer [with-extensions]]))
+
+(deftest register!
+  (testing "adds to registered extensions"
+    (with-extensions
+      (let [ext {:key :test/ext
+                 :before identity}]
+        (is (empty? (reset! sut/registered-extensions {})))
+        (is (some? (sut/register! ext)))
+        (is (= {:test/ext ext} @sut/registered-extensions))))))
+
+(deftest apply-extensions-before
+  (testing "executes `before` fn of registered extension for key"
+    (let [ext {:key :test/before
+               :before (fn [ctx]
+                         ;; Get the config from the job
+                         (assoc ctx ::value (get-in ctx [:job :test/before])))}
+          reg (sut/register sut/new-register ext)
+          job (bc/action-job "test-job" (constantly bc/success)
+                             {:test/before "config for extensions"})
+          res (sut/apply-extensions-before {:job job} reg)]
+      (is (= "config for extensions"
+             (::value res)))))
+
+  (testing "executes `before-job` multimethod for key"
+    (with-extensions
+      ;; Register a test extension
+      (defmethod sut/before-job :test/before [_ ctx]
+        (assoc ctx ::value (get-in ctx [:job :test/before])))
+      (is (some? (get-method sut/before-job :test/before)))
+      
+      (let [job (bc/action-job "test-job" (constantly bc/success)
+                               {:test/before "config for extensions"})
+            res (sut/apply-extensions-before {:job job})]
+        (is (= "config for extensions"
+               (::value res)))
+        (remove-method sut/before-job :test/before)))))
+
+(deftest apply-extensions-after
+  (testing "executes `after` fn of registered extension for key"
+    (let [ext {:key :test/after
+               :after (fn [ctx]
+                         ;; Get the config from the job
+                         (assoc ctx ::value (get-in ctx [:job :test/after])))}
+          reg (sut/register sut/new-register ext)
+          job (bc/action-job "test-job" (constantly bc/success)
+                             {:test/after "config for extensions"})
+          res (sut/apply-extensions-after {:job job} reg)]
+      (is (= "config for extensions"
+             (::value res)))))
+
+  (testing "executes `after-job` multimethod for key"
+    (with-extensions
+      ;; Register a test extension
+      (defmethod sut/after-job :test/after [_ ctx]
+        (assoc ctx ::value (get-in ctx [:job :test/after])))
+      (is (some? (get-method sut/after-job :test/after)))
+      
+      (let [job (bc/action-job "test-job" (constantly bc/success)
+                               {:test/after "config for extensions"})
+            res (sut/apply-extensions-after {:job job})]
+        (is (= "config for extensions"
+               (::value res)))
+        (remove-method sut/after-job :test/after)))))
+
+(deftest before-interceptor
+  (let [{:keys [enter] :as i} sut/before-interceptor
+        ext {:key :test/wrapped
+             :before (fn [ctx]
+                       (assoc ctx ::before? true))
+             :after  (fn [ctx]
+                       (assoc-in ctx [:job :result ::after?] true))}]
+    
+    (is (keyword? (:name i)))
+    
+    (testing "`enter` applies interceptors to job context"
+      (with-extensions
+        (let [job {:id "test-job"
+                   :test/wrapped {}}]
+          (is (some? (sut/register! ext)))
+          (is (= {::before? true
+                  :job job}
+                 (-> {}
+                     (emi/set-job-ctx {:job job})
+                     (enter)
+                     (emi/get-job-ctx)))))))))
+
+(deftest after-interceptor
+  (let [{:keys [enter] :as i} sut/after-interceptor
+        ext {:key :test/wrapped
+             :before (fn [ctx]
+                       (assoc ctx ::before? true))
+             :after  (fn [ctx]
+                       (assoc-in ctx [:job :result ::after?] true))}]
+    (is (keyword? (:name i)))
+
+    (testing "`enter` applies interceptors to job result"
+      (with-extensions
+        (let [job {:id "test-job"
+                   :test/wrapped {}
+                   :result {}}]
+          (is (some? (sut/register! ext)))
+          (is (= {::after? true}
+                 (-> {:event
+                      {:status :success
+                       :result {}}}
+                     (emi/set-job-ctx {:job job})
+                     (enter)
+                     (emi/get-job-ctx)
+                     :job
+                     :result))))))))
+
+(deftest update-job
+  (testing "applies arg fn to job in ctx"
+    (let [job {:id ::test-job}
+          ctx {:job job}
+          r (sut/update-job ctx assoc ::key ::value)]
+      (is (= {:id ::test-job
+              ::key ::value}
+             (:job r))))))
+
+(deftest set-value
+  (testing "updates job result"
+    (is (= {:id ::test-job
+            :result {::test ::value}}
+           (-> {:job {:id ::test-job}}
+               (sut/set-value ::test ::value)
+               :job)))))
